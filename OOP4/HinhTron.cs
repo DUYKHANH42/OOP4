2e459409f30c38560e22863ba4130ef727a049ac
DUYKHANH42 <kxtduykhanh@gmail.com>
2025-02-12 16:11:58 +0700

    Add project files.

---
+++ OOP4/HinhTron.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4
+{
+    public class HinhTron
+    {
+        
+        private double banKinh;
+
+        public HinhTron(double banKinh)
+        {
+            this.banKinh = banKinh;
+        }
+        public HinhTron()
+        {
+            banKinh = 1;
+        }
+            public double BanKinh
+        {
+            get { return banKinh; }
+            set { banKinh = value; }
+        }
+        public double TinhChuVi()
+        {
+            return 2 * Math.PI * banKinh;
+        }
+        public virtual double TinhDienTich()
+        {
+            return Math.PI * banKinh * 2;
+        }
+    }
+    public class HinhTru : HinhTron
+    {
+        private double chieucao;
+        public HinhTru(double banKinh, double chieucao) : base(banKinh)
+        {
+            this.chieucao=chieucao;
+        }
+        public double chieuCao
+        {
+            get { return chieucao; }
+            set { chieucao = value; }
+        }
+        public double TinhTheTich()
+        {
+            return base.TinhDienTich() * chieucao;
+        }
+
+        public override double TinhDienTich()
+        {
+            return (2*base.TinhChuVi())+(base.TinhChuVi()+chieucao);
+        }
+
+    }
+}
---
+++ OOP4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HinhTron C1 = new HinhTron(5);
+            HinhTru C2 = new HinhTru(5,10);
+            Console.WriteLine($"Hinh Tron C1: " +
+                $"\nban kinh = {C1.BanKinh}" +
+                $"\nchu vi= {C1.TinhChuVi()}" +
+                $"\ndien tich= {C1.TinhDienTich()}");
+            Console.WriteLine($"Hinh Tru C2: " +
+               $"\nban kinh = {C2.BanKinh}" +
+               $"\nchieu cao= {C2.chieuCao}" +
+               $"\nthe tich= {C2.TinhTheTich()}" +
+               $"\ndien tich= {C2.TinhDienTich()}");
+            Console.ReadKey();
+        }
+    }
+}
